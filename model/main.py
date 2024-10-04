1e82b5146832a10e9bbbf905f6880121e84e6d4f
abdullah <abdullahnaeemgill1724@gmail.com>
2024-10-04 13:28:25 +0500

    used startup

--- model/main.py
+++ model/main.py
@@ -1,47 +1,32 @@
 from fastapi import FastAPI
 from sqlmodel import Field, Session, SQLModel, create_engine, select
-from contextlib import asynccontextmanager
 
-
-class task(SQLModel, table=True):
+class Task(SQLModel, table=True):  
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
-  
-
-
 
 sqlite_url = f"postgresql://model_owner:fo4YjeSyOMP6@ep-proud-morning-a5aqk95t.us-east-2.aws.neon.tech/model?sslmode=require"
-
-
 engine = create_engine(sqlite_url)
-
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-    
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Load the ML model
-    create_db_and_tables()
-    yield
-   
 
+app = FastAPI()
 
-app:FastAPI = FastAPI(lifespan=lifespan)
-
-
+@app.on_event("startup")  
+def on_startup():
+    create_db_and_tables()  
 
 @app.post("/task/")
-def create_task(task: task):
+def create_task(task: Task):  
     with Session(engine) as session:
         session.add(task)
         session.commit()
         session.refresh(task)
         return task
 
-
-@app.get("/heroes/")
+@app.get("/tasks/") 
 def read_tasks():
     with Session(engine) as session:
-        tasks = session.exec(select(task)).all()
+        tasks = session.exec(select(Task)).all()  
         return tasks
